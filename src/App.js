f41b55d5f7587bc5089f42a8005d13f276d43d9c
lamnguyen8998 <84464387+lamnguyen8998@users.noreply.github.com>
2021-09-18 02:00:35 +0700

    BaiTapDatVe

--- src/App.js
+++ src/App.js
@@ -1,52 +1,10 @@
 import "./App.css";
-// import Baitap1 from './baitap1';
-// import Baitap2 from './baitap2';
-// import Baitap3 from './baptap3/baptap3';
-// import RederingElements from "./rendering-elements"
-// import HandlingEvents from './handling-events';
-// import ExampleHandleEvent from './handling-events/example';
-// import State from './state';
-// import ExampleCar from './example-car'
-// import Listskeys from './lists-keys';
-// import ListsFilms from './baitap4';
-// import Communication from './communication';
-// import LiftingStateUpCart from './shopping-cart'
-// import Home from './user-management'
-// import HomeRedux from './user-management-redux'
-// import Lifecycle from "./lifecycle";
-// import OanTuXi from "./BaiTapRedux/BaiTapOanTuXi";
-// import FormValidation from "./form-validation";
+
 import BaiTapBookingTicket from "./BaiTapRedux/BaiTapBookingTicket/BaiTapBookingTicket";
 
 function App() {
   return (
     <div>
-      {/* <Baitap1 /> */}
-      {/* <Baitap2 /> */}
-      {/* <Baitap3 /> */}
-      {/* <RederingElements /> */}
-
-      {/* <HandlingEvents /> */}
-
-      {/* <ExampleHandleEvent /> */}
-
-      {/* <State /> */}
-
-      {/* <ExampleCar /> */}
-
-      {/* <Listskeys /> */}
-
-      {/* <ListsFilms /> */}
-      {/* <Communication /> */}
-
-      {/* <LiftingStateUpCart /> */}
-
-      {/* <Home /> */}
-
-      {/* <HomeRedux /> */}
-      {/* <Lifecycle /> */}
-      {/* <OanTuXi />
-      <FormValidation /> */}
       <BaiTapBookingTicket />
     </div>
   );
